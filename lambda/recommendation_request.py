5baf17212c57957a8ae8445857938c114ba3e9f2
ernraff <103540977+ernraff@users.noreply.github.com>
2024-07-07 16:17:58 -0400

    Update and rename lambda.py to recommendation_request.py

---
+++ lambda/recommendation_request.py
@@ -0,0 +1,52 @@
+import json
+import boto3
+import os
+import time
+import sys
+
+state_machine_arn = 'arn:aws:states:us-east-1:604242335225:stateMachine:MyStateMachine-xnsut9h02'
+
+### This lambda handler sends an async request to create labels for the video file and return the job execution id.
+def lambda_handler(event, context):
+    region_name = 'us-east-1'
+    s3_client = boto3.client('s3')
+    bucket_name = 'techjam2024stack-videobucket6ed8e1af-xlqpbzfoirbp'
+    video = event['pathParameters']['fileName']
+    input_data = {
+        "fileName": video
+    }
+    
+    # Initialize the Step Functions client
+    step_function_client = boto3.client('stepfunctions')
+    
+    execution_arn = None
+    
+    try:
+        # Start the Step Function execution
+        response = step_function_client.start_execution(
+            stateMachineArn=state_machine_arn,
+            input=json.dumps(input_data)
+        )
+    
+        # Extract the execution ARN from the response
+        execution_arn = response['executionArn']
+        print(f"Started execution with ARN: {execution_arn}")
+        # Optionally return execution ARN or other response details
+
+    except Exception as e:
+        print(f"Error starting Step Function execution: {e}")
+        
+    # Describe the Step Function execution to get its details
+    response = step_function_client.describe_execution(
+        executionArn="arn:aws:states:us-east-1:604242335225:execution:MyStateMachine-xnsut9h02:80995c77-7971-4184-ad4a-578962e00f08"
+    )
+
+    # Extract and print the execution status
+    execution_output = response["output"]
+    print(f"Execution ID '{execution_arn}' execution_output: {execution_output}")
+ 
+
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps(execution_arn)
+    }
